54c977581ea5747051b31390b9b05a7f15b12c66
Éric Lemoine <eric.lemoine@gmail.com>
2014-04-14 20:24:18 +0200

    Update

--- foss4g2013/examples/interaction-control.js
+++ foss4g2013/examples/interaction-control.js
@@ -5,7 +5,7 @@ var view = new ol.View2D({
 
 var layer = new ol.layer.Tile({
   source: new ol.source.BingMaps({
-    key: 'Ar33pRUvQOdESG8m_T15MUmNz__E1twPo42bFx9jvdDePhX0PNgAcEm44OVTS7tt',
+    key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
     style: 'Aerial'
   })
 });
