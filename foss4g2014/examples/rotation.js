869e853612b625a6c6f4b45db9807c767a882a88
Éric Lemoine <eric.lemoine@gmail.com>
2014-09-02 14:34:08 +0200

    [foss4g2014] More complex rotation example

--- foss4g2014/examples/rotation.js
+++ foss4g2014/examples/rotation.js
@@ -33,13 +33,45 @@ var vectorLayer = new ol.layer.Vector({
   }
 });
 
+var greenSightStyle = new ol.style.Style({
+  image: new ol.style.Icon({
+    src: 'data/sight-2.png',
+    anchor: [0.5, 1]
+  })
+});
+var pinkSightStyle = new ol.style.Style({
+  image: new ol.style.Icon({
+    src: 'data/sight-2-pink.png',
+    anchor: [0.5, 1]
+  })
+});
+
+var marker = new ol.Feature(
+    new ol.geom.Point([-8620815.572252877, 2078464.5633878764]));
+marker.setStyle(greenSightStyle);
+marker.setId('marker');
+vectorLayer.getSource().addFeature(marker);
+
 var map = new ol.Map({
   target: 'map',
   layers: [rasterLayer, vectorLayer],
   view: new ol.View({
-    center: [-8692416.162680985, 1821599.940530574],
+    center: marker.getGeometry().getCoordinates(),
     zoom: 6
   })
+});
+
+map.on('click', function(e) {
+  var feature = map.forEachFeatureAtPixel(e.pixel, function(feature, layer) {
+    if (feature.getId() == 'marker') {
+      return feature;
+    }
+  });
+  if (feature) {
+    feature.setStyle(pinkSightStyle);
+  } else {
+    marker.setStyle(greenSightStyle);
+  }
 });
 
 
@@ -57,5 +89,28 @@ document.onkeydown = function(e) {
     // "l" key
     rotation -= 0.01;
     view.setRotation(Math.max(rotation, -3.14159));
+  } else if (e.keyCode == 70 || e.keyCode == 66) {
+    // "f" key - fly to Mexico
+    // "b" key - fly back to 
+    var duration = 2000;
+    var start = +new Date();
+    var pan = ol.animation.pan({
+      duration: duration,
+      source: view.getCenter(),
+      start: start
+    });
+    var bounce = ol.animation.bounce({
+      duration: duration,
+      resolution: 4 * view.getResolution(),
+      start: start
+    });
+    map.beforeRender(pan, bounce);
+    if (e.keyCode == 70) {
+      // Mexico
+      view.setCenter([-11405013.422465283, 2746182.234668064]);
+    } else {
+      // Jamaica
+      view.setCenter(marker.getGeometry().getCoordinates());
+    }
   }
 };
