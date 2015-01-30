feb6b45e2505eede9c513b08195a0222d515c321
Éric Lemoine <eric.lemoine@gmail.com>
2015-01-30 16:18:46 +0100

    [fossdem2015] Update rotation example

--- fossdem2015/examples/rotation.js
+++ fossdem2015/examples/rotation.js
@@ -1,8 +1,8 @@
 var rasterLayer = new ol.layer.Tile({
-  source: new ol.source.BingMaps({
-    key: 'Ak-dzM4wZjSqTlzveKz5u0d4IQ4bRzVI309GxmkgSVr1ewS6iPSrOvOKhA-CJlm3',
-    imagerySet: 'Aerial',
-    preload: Infinity
+  preload: 2,
+  source: new ol.source.MapQuest({
+    url: 'http://localhost:8080/tiles/0/tiles/{z}/{x}/{y}',
+    layer: 'osm'
   })
 });
 
